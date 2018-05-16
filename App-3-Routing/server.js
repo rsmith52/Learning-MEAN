922bfffce8a8fdad6742a008468844f382cea50f
ray <rsmith52@wisc.edu>
2018-05-16 13:19:30 -0500

    App 3 routes to different pages, utilizes middleware to check params, and uses params in routes

--- App-3-Routing/server.js
+++ App-3-Routing/server.js
@@ -15,6 +15,33 @@ app.get('/', function (req, res) {
 // Get an instance of the router
 var adminRouter = express.Router();
 
+/* MIDDLEWARE */
+
+// Route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+
+  // Log each request to the console
+  console.log(req.method, req.url);
+
+  // Continue doing what we were doing and go to the route
+  next();
+});
+
+// Route middleware for parameters
+adminRouter.param('name', function(req, res, next, name) {
+  // Do validation on name here
+  // Log something to know it works
+  console.log('doing some name validations on ' + name);
+
+  // Once validation is done save the new item in the req
+  req.name = name;
+
+  // Go to the next thing
+  next();
+})
+
+/* ROUTES */
+
 // Admin main page. The dashboard (http://localhost:1337/admin)
 adminRouter.get('/', function(req, res) {
   res.send('I am the dashboard!');
@@ -25,6 +52,13 @@ adminRouter.get('/users', function(req, res) {
   res.send('I show all the users!');
 });
 
+// Individual user pages (https://localhost:1337/admin/users/:name)
+adminRouter.get('/users/:name', function(req, res) {
+//  res.send('Hello ' + req.params.name + '!');
+  // Replaced above line after adding validation on name
+  res.send('Hello ' + req.name + '!');
+});
+
 // Posts page (http://localhost:1337/admin/posts)
 adminRouter.get('/posts', function(req, res) {
   res.send('I show all the posts!');
@@ -32,6 +66,22 @@ adminRouter.get('/posts', function(req, res) {
 
 // Apply the routes to our application
 app.use('/admin', adminRouter);
+
+/* LOGIN ROUTE */
+
+app.route('/login')
+  // Show the form (GET http://localhost:1337/login)
+  .get(function(req, res) {
+    res.send('this is the login form');
+  })
+
+  // Process the form (POST http://localhost:1337/login)
+  .post(function(req, res) {
+    console.log('processing');
+    res.send('processing the login form!');
+  });
+
+/* START SERVER */
 
 // Start the server
 app.listen(1337);
