ff7aa1d5f3b8f13255bc74b914c5036c2ea7aebe
ray <rsmith52@wisc.edu>
2018-05-16 11:18:20 -0500

    App 1 does not use express - feeds basic html page

--- App 1/server.js
+++ App 1/server.js
@@ -1,1 +1,24 @@
 console.log('test log');
+// Get the http and filesystem modules
+var http = require('http'),
+  fs = require('fs');
+
+// Create our server using http modules
+http.createServer(function(req, res) {
+
+  // Write to our server. Set configuration for the response
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Access-Control-Allow-Origin' : '*'
+  });
+
+  // Grab the index.html file using fs
+  var readStream = fs.createReadStream(__dirname + '/index.html');
+
+  // Send the index.html file to our user
+  readStream.pipe(res);
+
+}).listen(1337);
+
+// Tell ourselves what's happening
+console.log('Visit me at http://localhost:1337');
